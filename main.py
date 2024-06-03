3deb23af369dcada057c1f93d580dc4870c0dbf4
Klymchuk Bohdan <ipzk211_kba@student.ztu.edu.ua>
2024-06-03 19:15:28 +0300

    deployment fixes

--- main.py
+++ main.py
@@ -71,16 +71,16 @@ app.include_router(collection.router)
 app.include_router(quiz.router)
 
 
-origins = [
-    "*"
-]
+# origins = [
+#     "*"
+# ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
